5e28255351818625650e2becfad7046c8b00282b
tatsuyuki-morishita <tatsuyuki52505250@gmail.com>
2021-07-12 00:02:45 +0900

    vimrc大幅修正

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ ln -sf ~/.vim_setting/.vim ~/.vim
 ln -sf ~/.vim_setting/.vimrc ~/.vimrc
 ln -sf ~/.vim_setting/.bashrc ~/.bashrc
 ln -sf ~/.vim_setting/.bash_profile ~/.bash_profile
+
+#OSごとの初期設定を以下に記述
